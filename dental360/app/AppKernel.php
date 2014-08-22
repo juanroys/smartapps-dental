b04778dc4aacc864fc3be8f1eacc4bd16428f94d
waruano9212 <waruano9212@gmail.com>
2014-08-22 15:37:24 +0000

    Creados bundle de Historia Clinica y bundle de Usuarios

--- dental360/app/AppKernel.php
+++ dental360/app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new SmartApps\HistClinicaBundle\HistClinicaBundle(),
+            new SmartApps\UsuarioBundle\UsuarioBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
