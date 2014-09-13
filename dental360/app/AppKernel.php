f98f89f7dc54fab377239c168b8df46186e28a48
waruano9212@gmail.com <waruano9212@gmail.com@e3f4badf-d30d-a22e-31c8-aae0d406b6c8>
2014-09-13 04:12:14 +0000

    instalando bootstrap

--- dental360/app/AppKernel.php
+++ dental360/app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Ideup\SimplePaginatorBundle\IdeupSimplePaginatorBundle(),
+            new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
